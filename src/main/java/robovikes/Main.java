cbabea78858238b89dbffedf8b2bf2d296f506c0
cmoliverio <cmoliverio.10@gmail.com>
2024-06-06 19:19:47 -0500

    Making custom, schedulable subsystem

---
+++ src/main/java/robovikes/Main.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package robovikes;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public final class Main {
+    private Main() {}
+    
+    public static void main(String... args) {
+        // humans can't perceive much faster than 16 milliseconds
+        // set refresh to be about twice as fast
+        Robot r = new Robot(0.007d);
+        RobotBase.startRobot(() -> r); // pointer hack to have customizeable refresh rate
+        r.close(); // remove warning
+    }
+}
---
+++ src/main/java/robovikes/Robot.java
@@ -0,0 +1,85 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package robovikes;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.RobotContainer;
+import robovikes.subsystems.Arm;
+
+public class Robot extends TimedRobot {
+    private Command m_autonomousCommand;
+    
+    private RobotContainer m_robotContainer;
+    private Arm new_arm = new Arm();
+    
+    public Robot(double refreshRate) {
+        super(refreshRate);
+    }
+    
+    @Override
+    public void robotInit() {
+        m_robotContainer = new RobotContainer();
+    }
+    
+    @Override
+    public void robotPeriodic() {
+        //CommandScheduler.getInstance().run(); 
+    }
+    
+    @Override
+    public void disabledInit() {}
+    
+    @Override
+    public void disabledPeriodic() {}
+    
+    @Override
+    public void disabledExit() {}
+    
+    @Override
+    public void autonomousInit() {
+        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+        
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.schedule();
+        }
+    }
+    
+    @Override
+    public void autonomousPeriodic() {}
+    
+    @Override
+    public void autonomousExit() {}
+    
+    @Override
+    public void teleopInit() {
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.cancel();
+        }
+    }
+    
+    @Override
+    public void teleopPeriodic() {
+        // teleop 
+    }
+    
+    @Override
+    public void teleopExit() {}
+    
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+    
+    @Override
+    public void testPeriodic() {}
+    
+    @Override
+    public void testExit() {}
+    
+    @Override
+    public void simulationPeriodic() {}
+}
